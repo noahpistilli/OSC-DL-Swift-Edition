efd8155ba914b95555fff3f0f9e2b7956dcacb0c
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-06-17 22:42:22 -0700

    Initial Commit

---
+++ Open_Shop_Channel_DownloaderApp.swift
@@ -0,0 +1,16 @@
+//
+//  Open_Shop_Channel_DownloaderApp.swift
+//
+//  Created by Noah Pistilli on 2021-06-15.
+//
+
+import SwiftUI
+
+@main
+struct Open_Shop_Channel_DownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OSC_DB()
+        }
+    }
+}
